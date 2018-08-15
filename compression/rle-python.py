8a8094842422022539d153bd4e1cc1d27197fcfe
molecule <molecule@berkeley.edu>
2018-08-15 12:05:11 -0700

    prints

--- compression/rle-python.py
+++ compression/rle-python.py
@@ -11,3 +11,16 @@ print(imgfile.format, imgfile.size, imgfile.mode)
 # more info: https://pillow.readthedocs.io/en/5.1.x/handbook/concepts.html#concept-modes
 
 #imgfile.show(); # opens preview w/ the image. Appears to be inverted colors.
+
+# Returns the contents of this image as a sequence object containing pixel values. 
+# The sequence object is flattened, so that values for line one follow directly 
+# after the values of line zero, and so on.
+imgdata = imgfile.getdata()
+
+print("len of imgdata: " + str(len(imgdata)))
+
+#for (int i = 0; i < )
+print(imgdata[0])
+
+# To convert it to an ordinary sequence (e.g. for printing), use 
+#print(list(imgfile.getdata()))
