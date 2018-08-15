b18d990b13fb3da8e8e37f22652b1d8b2088dad1
molecule <molecule@berkeley.edu>
2018-08-15 09:19:24 -0700

    comment out img show line and add link to tutorial

--- compression/rle-python.py
+++ compression/rle-python.py
@@ -1,5 +1,8 @@
 from PIL import Image
+# useful: http://pillow.readthedocs.io/en/3.3.x/handbook/tutorial.html
 
 imgfile = Image.open("trip_start_1_44.xbm")
 
 print imgfile
+
+#imgfile.show(); # opens preview w/ the image. Appears to be inverted colors.
