cec2be6f55a83742eb501bd672ae106c963e60a1
molecule <molecule@berkeley.edu>
2018-08-15 09:24:48 -0700

    add info on image mode

--- compression/rle-python.py
+++ compression/rle-python.py
@@ -4,6 +4,10 @@ from PIL import Image
 imgfile = Image.open("trip_start_1_44.xbm")
 
 print imgfile
-print(imgfile.format, imgfile.size, imgfile.mode)
+# size attribute is a 2-tuple containing width and height (in pixels)
+# "mode" defines the number and names of the bands in the image, and also the pixel type and depth
+print(imgfile.format, imgfile.size, imgfile.mode) 
+# mode = 1, (1-bit pixels, black and white, stored with one pixel per byte)
+# more info: https://pillow.readthedocs.io/en/5.1.x/handbook/concepts.html#concept-modes
 
 #imgfile.show(); # opens preview w/ the image. Appears to be inverted colors.
