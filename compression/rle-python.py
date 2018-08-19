ca0af586a610b745ea90c4ea36b544d19982174d
molecule <molecule@berkeley.edu>
2018-08-19 10:03:55 -0700

    confirmed info re datastructure

--- compression/rle-python.py
+++ compression/rle-python.py
@@ -20,12 +20,27 @@ imgdata = imgfile.getdata()
 # https://github.com/python-pillow/Pillow/blob/5e1a528eb25fdaa5d2761ce7f13c1bb4d97cc1eb/selftest.py
 print("len of imgdata: " + str(len(imgdata)))
 
+# Notes on imgdata structure:
+# Each element at an index represents a pixel.
+# For some reason, when I print them out using python, an individual element
+# prints out as 8 bits: either 255 (int) or 0b11111111 (binary).
+# But, if my test file has: 0x81  (0b10000001) as the very first entry, what gets correctly printed out is:
+# i: 0, imgdata[i]: 0b11111111
+# i: 1, imgdata[i]: 0b0
+# i: 2, imgdata[i]: 0b0
+# i: 3, imgdata[i]: 0b0
+# i: 4, imgdata[i]: 0b0
+# i: 5, imgdata[i]: 0b0
+# i: 6, imgdata[i]: 0b0
+# i: 7, imgdata[i]: 0b11111111
+
 rle = {}
 currentPixel = 0
 runLength = 0
 index = 0
 for i in range(0, len(imgdata)):
-    print("i: " + str(i) + ", imgdata[i]: " + str(bin(imgdata[i])) + ", currentPixel: " + str(currentPixel))
+    print("i: " + str(i) + ", imgdata[i]: " + str(bin(imgdata[i])) + 
+        ", currentPixel: " + str(currentPixel))
     if (imgdata[i] == currentPixel):
         runLength += 1
     else:
@@ -33,7 +48,6 @@ for i in range(0, len(imgdata)):
       index += 1
       runLength = 0
       currentPixel = imgdata[i]
-    #print(imgdata[i])
 print(rle)
 
 # To convert it to an ordinary sequence (e.g. for printing), use 
