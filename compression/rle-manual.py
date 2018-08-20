8f1520662c9dca1bc7b7b4b46f31be5d6d3ee59e
molecule <molecule@berkeley.edu>
2018-08-20 12:05:47 -0700

    more notes on xbm file format

--- compression/rle-manual.py
+++ compression/rle-manual.py
@@ -9,3 +9,10 @@ from struct import *
 #print(sys.byteorder) #returns "little"
 img  = open("test.xbm", "r") # r = read-only
 print(img.read())
+
+# might be useful: https://auth0.com/blog/image-processing-in-python-with-pillow/
+
+# WAIT:
+#   each byte in the array contains the information for eight pixels, 
+#   with the upper left pixel in the bitmap represented by the low bit of the first byte in the array.
+#   low bit means if I have 0b10010011, then that "far right" 1 is the upper left pixel?
